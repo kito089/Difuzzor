8e170e5d2effe48eac571bde3293a4de6015c771
kito089 <jezarinan@gmail.com>
2025-10-27 09:36:40 -0600

    pequeño error en app navigator

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -12,13 +12,12 @@ const Stack = createNativeStackNavigator();
 export default function AppNavigator(){
   return (
     console.log("AppNavigator renderizado"),
-    <Stack.Navigator initialRouteName="Clubs" screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="Crud" screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Market" component={MarketScreen} />
       <Stack.Screen name="Profile" component={ProfileScreen} />
       <Stack.Screen name="Prueba" component={PruebaScreen} />
       <Stack.Screen name="Crud" component={CrudScreen} />
-      <Stack.Screen name="Agred" component={AgredScreen} />
     </Stack.Navigator>
   )
 }
