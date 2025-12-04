e121f1865fdcae4261ac8340ce9806dc0f8fc1f2
ThreeBook3458 <aronhuertayan@gmail.com>
2025-12-04 11:53:25 -0600

    dadawdawwdawdawdawdawd Queremos que chantis biale la pelusa

--- src/navigation/indexNavigation.js
+++ src/navigation/indexNavigation.js
@@ -1,13 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { useSession } from "../utils/SessionContext";
 import AppNavigator from "./AppNavigator";
-
+import AuthNavigator from "./AuthNavigator";
 export default function IndexNavigation() {
   const { userauth } = useSession();
   // TODO: Revertir a {userauth ? <AppNavigator /> : <AuthNavigator />} cuando se implemente login
   return (
     <NavigationContainer>
-      <AppNavigator />
+      {userauth ? <AppNavigator /> : <AuthNavigator />}
     </NavigationContainer>
   );
 }
